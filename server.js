b771a5e3df7688b49a25e8e968b6a32a015241a9
ams1997 <ams281997@gmail.com>
2020-11-22 23:59:00 +0530

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
+const initRoutes = require('./routes/web');
 const PORT = process.env.PORT || 4000;
 
 //assets
@@ -13,21 +14,8 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs');
 
-
 //routes
-app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/cart', (req, res) => {
-    res.render('customers/cart');
-})
-app.get('/login', (req, res) => {
-    res.render('auth/login');
-})
-app.get('/register', (req, res) => {
-    res.render('auth/register');
-})
-
+require('./routes/web')(app)
 
 //listen port
 app.listen(PORT, () => {
